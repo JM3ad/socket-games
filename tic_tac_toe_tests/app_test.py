0633e3109ab80e46d904e2f8ddd6b3c06c4d005e
Jack Mead <jack-mead@hotmail.co.uk>
2022-03-01 08:55:56 +0000

    Format the test files too

--- tic_tac_toe_tests/app_test.py
+++ tic_tac_toe_tests/app_test.py
@@ -3,25 +3,23 @@ import json
 
 from tic_tac_toe.app import app
 
+
 @pytest.fixture
 def test_app():
     return app
+
 
 @pytest.mark.asyncio
 async def test_websocket(test_app):
     test_client = test_app.test_client()
     game_id = "abcdef"
-    data = json.dumps({
-        "message_type": "move",
-        "game_id": game_id,
-        "move": "top-right"
-    })
-    await test_client.get(f'/game/{game_id}')
+    data = json.dumps({"message_type": "move", "game_id": game_id, "move": "top-right"})
+    await test_client.get(f"/game/{game_id}")
 
-    async with test_client.websocket('/ws') as test_websocket:
+    async with test_client.websocket("/ws") as test_websocket:
         await test_websocket.send(data)
         result = await test_websocket.receive()
-    
+
     json_result = json.loads(result)
-    state = json_result['state']
+    state = json_result["state"]
     assert "X" in state
